f6fea2380db93243bae36e520fe23b1fb462638c
pchmiele <przemyslaw.chmielewski.gda@gmail.com>
2016-05-15 22:07:24 +0200

    Added FsharpWorker project

---
+++ AkkaNetFsharp/FsharpWorker/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace FsharpWorker.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("FsharpWorker")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FsharpWorker")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("14a89afc-5c96-4dfc-88e6-26ac6a6cd13e")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ AkkaNetFsharp/FsharpWorker/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open Akka
+open Akka.Actor
+open Akka.FSharp
+open Akka.Configuration
+open Akka.Routing
+open Akka.Cluster
+open FsharpCommon
+
+let system = System.create "clusterexample" <| ConfigurationFactory.Load()
+
+[<EntryPoint>]
+let main argv = 
+    System.Console.ReadKey() |> ignore
+    0 
